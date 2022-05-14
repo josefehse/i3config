634ed67dac603cd7a33525ebe6ec78afdf73b43e
Jose Fehse <jose@fehse.ca>
2022-05-14 10:22:34 -0400

    multiple updates

---
+++ fix_panning.sh
@@ -0,0 +1,2 @@
+xrandr --output DP-0 --off --output DP-1 --mode 2560x1440 --fb 2560x1440 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-3 --mode 2560x1080 --fb 2560x1080 --pos 0x40 --rotate normal --output DP-4 --mode 1920x1080 --pos 5120x40 --rotate normal --fb 1920x1080 --output DP-5 --off --output DP-6 --off
+
